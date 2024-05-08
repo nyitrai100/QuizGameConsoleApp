04532af31be8c0cf66faa93dabbd6a46ddbbf1d3
nyitraibence <nyitrai100@gmail.com>
2024-05-08 15:15:36 +0100

    QuizGame done!

--- QuizGameConsoleApp/Program.cs
+++ QuizGameConsoleApp/Program.cs
@@ -2,7 +2,51 @@
 
 string[] text = File.ReadAllLines("questions.txt");
 
+List<string> questions = new List<string>();
+List<string> answers = new List<string>();
+
 for (int i = 0; i < text.Length; i++)
 {
-    Console.WriteLine(text[i]);
+    if ((i % 4) == 0)
+        questions.Add(text[i]);
+    else
+    {
+        answers.Add(text[i]);
+    }
 }
+
+    int questionsIndex = 0;
+    int answerIndex = 0;
+    int score = 0;
+
+    while (questionsIndex < questions.Count)
+    {
+        Console.WriteLine(questions[questionsIndex]);
+        questionsIndex++;
+
+        int correctAnswerIndex = 0;
+
+        for (int i = 0; i < 3; i++)
+        {
+            if (answers[answerIndex].StartsWith(">"))
+                correctAnswerIndex = i + 1;
+            Console.WriteLine(i + 1 + "." + answers[answerIndex].Replace(">", ""));
+            answerIndex++;
+        }
+
+        int answer = int.Parse(Console.ReadLine());
+        if (answer == correctAnswerIndex)
+        {
+            Console.WriteLine("Correct");
+            score++;
+        }
+        else
+        {
+            Console.WriteLine("Incorrect");
+        }
+        
+    }
+
+Console.WriteLine("End of quiz! Your score was " + score);
+
+
